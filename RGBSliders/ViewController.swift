8c2735b1ce79c4b87268f2b5b1937fbafd2bc654
Taylor Boren <taylorboren@teletech.com>
2015-06-20 08:34:06 -0600

    added user defaults

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var red: UISlider!
+    @IBOutlet weak var green: UISlider!
+    @IBOutlet weak var blue: UISlider!
+    @IBOutlet weak var preview: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         preview.layer.borderColor = UIColor.blackColor().CGColor
         preview.layer.borderWidth = 1
+        let defaults = NSUserDefaults.standardUserDefaults()
+        red.value = defaults.floatForKey("red")
+        green.value = defaults.floatForKey("green")
+        blue.value = defaults.floatForKey("blue")
+        updateBackgroundColor()
         
     }
     override func didReceiveMemoryWarning() {
@@ -21,13 +32,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBOutlet weak var red: UISlider!
-    @IBOutlet weak var green: UISlider!
-    @IBOutlet weak var blue: UISlider!
-    @IBOutlet weak var preview: UIView!
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "openColor"){
+            let newViewController = segue.destinationViewController as! UIViewController
+            newViewController.view.backgroundColor = preview.backgroundColor
+        }
+    }
     
     @IBAction func updateBackgroundColor(){
         preview.backgroundColor = UIColor(red: CGFloat(red.value), green: CGFloat(green.value), blue: CGFloat(blue.value), alpha: 1.0)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setFloat(red.value, forKey: "red")
+        defaults.setFloat(green.value, forKey: "green")
+        defaults.setFloat(blue.value, forKey: "blue")
+        defaults.synchronize() // save values to device
     }
 
 }
